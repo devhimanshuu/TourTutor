0139dd8411bdc4924fe5fb7696e4039f6aaf1466
devhimanshuu <devhimanshuu@gmail.com>
2024-03-18 15:33:18 +0530

    App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import "./App.css";
+import { Navbar } from "./components/Navbar/navbar";
+import { Home } from "./components/Home/Home";
+import { Footer } from "./components/Footer/Footer";
+import { Main } from "./components/Main/main";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Home />
+      <Main />
+      <Footer />
+    </>
   );
-}
+};
 
 export default App;
